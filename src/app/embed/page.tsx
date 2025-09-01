c581c114152bec1648c2b7ff626336dfc893c91a
EduardoHenriqueDev <eduhenrimacedo@gmail.com>
2025-09-01 16:23:19 -0300

    GLV

--- src/app/embed/page.tsx
+++ src/app/embed/page.tsx
@@ -7,13 +7,10 @@ const EmbedPage: FC = () => {
     return (
         <div className="relative flex min-h-screen bg-[#121212]">
 
-            {/* Drawer */}
             <Drawer />
 
-            {/* Conteúdo da planilha */}
             <div className="flex flex-col flex-1">
 
-                {/* Container do iframe preenchendo toda altura restante */}
                 <div className="flex-1 w-full">
                     <iframe
                         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQy0Ylsabv2tjJxetR06WJyEaVvQdAtETIxqAAm6fK2JPOePDGpQsp5tWj8d894Eg/pubhtml?widget=true&amp;headers=false"
